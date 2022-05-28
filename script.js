fefc08f614af21eb235caf574b89e89abd54e74e
nimely111 <samuelehnimely@gmail.com>
2022-05-28 16:45:30 +0000

    added a class called options to the select tag, and testing the function I just completed

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const membersLinked = document.getElementById('members-link');
 membersLinked.onchange = function () {
     let userOption = this.options[this.selectedIndex];// selectedIndex in a js property
+    if(userOption.value != "nothing"){
+        window.open(this.options[this.selectedIndex],)
+    }
 }
