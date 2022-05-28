cabaecbefcd6796aace8cc167538def9592b407f
nimely111 <samuelehnimely@gmail.com>
2022-05-28 16:33:19 +0000

    added the this keyword to point to each of the specified click of options that the user will click

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-const options = document.getElementById('options');
+const membersLinked = document.getElementById('members-link');
+membersLinked.onchange = function () {
+    let userOption = this.options[this.selectedIndex];// selectedIndex in a js property
+}
