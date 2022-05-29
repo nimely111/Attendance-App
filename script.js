f3fd092727df2b53f1df885cefb1f043353a7278
nimely111 <samuelehnimely@gmail.com>
2022-05-29 03:08:35 +0000

    modified relative atribute

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const membersLinked = document.getElementById('members-link');
 membersLinked.onchange = function () {
     let userOption = this.options[this.selectedIndex];// selectedIndex in a js property
     if(userOption.value != "nothing"){
-        window.open(this.options[this.selectedIndex],)
+        window.open(userOption.value, "attedance app", "");
     }
 }
