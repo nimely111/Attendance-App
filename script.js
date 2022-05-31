94ba44e8809d122059bf4f7c808cde35b1eb7d56
nimely111 <samuelehnimely@gmail.com>
2022-05-31 19:21:33 +0000

    remove all internal links from the option value attribute , and got rid of the entire js file. I also added a new attribute of size and change the name attribute to links

--- script.js
+++
@@ -1,7 +0,0 @@
-// const membersLinked = document.getElementById('members-link');
-// membersLinked.onchange = function () {
-//     let userOption = this.options[this.selectedIndex];// selectedIndex in a js property
-//     if(userOption.value != "nothing"){
-//         window.open(userOption.value, "attedance app", "");
-//     }
-// }
