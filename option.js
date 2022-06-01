d22b58cef4b179d58d3dcb6acd85644426961f2b
nimely111 <samuelehnimely@gmail.com>
2022-06-01 07:38:53 +0000

    I created three variable for the testing. the variables are the option_text, option_value, and to check if a specific option will be display to the console when selected

--- option.js
+++ option.js
@@ -1,3 +1,7 @@
 Array.from(document.querySelector('#colors-dropdown').options).forEach((option_element) => {
-    
+    let option_text = option_element.option_text.value;
+    let option_value = option_element.option_value;
+    let is_option_selected = option_element.value;
+
+
 })
