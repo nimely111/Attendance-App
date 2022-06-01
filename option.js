cb0c30fe171b16096f6842c97d4ad759e6888bd1
nimely111 <samuelehnimely@gmail.com>
2022-06-01 07:43:12 +0000

    re wrote the code for getting each value by a soecific target

--- option.js
+++ option.js
@@ -1,7 +1,9 @@
 Array.from(document.querySelector('#colors-dropdown').options).forEach((option_element) => {
-    let option_text = option_element.option_text.value;
-    let option_value = option_element.option_value;
-    let is_option_selected = option_element.value;
+    let option_text = option_element.text;
+    let option_value = option_element.value;
+    let is_option_selected = option_element.selected;
+
+    
 
 
 })
