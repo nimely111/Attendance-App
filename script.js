2ef0f6a347b6a659cb0246aab87f9a589d4af3df
nimely111 <samuelehnimely@gmail.com>
2022-06-08 03:58:41 +0000

    debuging and finding which solution to be used for the HTML select object to listen for a click event and output result in the browser

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-// const selectEachMember = document.getElementById('members-link');
-
-// for(let i = 0; i < selectEachMember.length; i++){
-
-//     console.log(selectEachMember[i]);
-
-
-//     selectEachMember[i].addEventListener('click', (e)=>{
-//         let clicked_element = e.target;
-//         console.log(clicked_element);
-//         console.log("Working");
-
-//     })
-
-// }
-
 // converting the select object to array using the Array.from() method
-const selectMembersOption = Array.from(document.querySelector('#members-option').options).forEach(function(option_element){
-    // initializing the variables to be loop over
-    let  option_text = option_element.text;
-    let option_value = option_element.value;
-    let is_option_selected = option_element.selected;
+// const selectMembersOption = Array.from(document.querySelector('#members-option').options).forEach(function(option_element){
+//     // initializing the variables to be loop over
+//     let  option_text = option_element.text;
+//     let option_value = option_element.value;
+//     let is_option_selected = option_element.selected;
 
-    console.log('Options text: ' +option_text);
-    console.log('Options value: ' +option_value);
-    console.log('Options selected: ' +(is_option_selected === true ? 'Yes' :  'No'));
+//     console.log('Options text: ' +option_text);
+//     console.log('Options value: ' +option_value);
+//     console.log('Options selected: ' +(is_option_selected === true ? 'Yes' :  'No'));
 
-    console.log('\n\r');
-    
+//     console.log('\n\r');
+// })
+
+const selectMembersOption = document.querySelector('select');
+const output = document.querySelector('#result');
+selectMembersOption.addEventListener('change', () =>{
+    output.innerText = selectMembersOption.value;
 })
